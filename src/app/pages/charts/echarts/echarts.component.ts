a09432785d13e584a881caa8515550d5223f5e28
FasterThanLigh1 <quan.uminh1@gmail.com>
2025-05-03 01:52:20 +0700

    Update UI

--- src/app/pages/charts/echarts/echarts.component.ts
+++ src/app/pages/charts/echarts/echarts.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'ngx-echarts',
   styleUrls: ['./echarts.component.scss'],
   templateUrl: './echarts.component.html',
 })
-export class EchartsComponent {}
+export class EchartsComponent implements OnInit {
+  data: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    // Make HTTP request when component initializes
+    this.getData();
+  }
+
+  getData() {
+    this.http.get('http://127.0.0.1:5000/test')
+      .subscribe(
+        (response) => {
+          this.data = response;
+          console.log('Data received:', this.data);
+        },
+        (error) => {
+          console.error('Error fetching data:', error);
+        },
+      );
+  }
+}
